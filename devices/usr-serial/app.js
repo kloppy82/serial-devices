3e0a86ffdaaacb773a7c6900b51bb1b74ebaee5a
Markus Lempen <markus.lempen@damedics.com>
2018-06-12 15:06:55 +0200

    Rewrote device discovery

--- devices/usr-serial/app.js
+++ devices/usr-serial/app.js
@@ -1,5 +1,11 @@
 const usr=require('./discoverusr');
 //var serial = require('./serialio');
-
-usr.discoverDevices();
-//
+usr.discoverDevices(500,(input)=>{
+	if (input.length>0){
+		console.log("Found these devices:")
+		for (var i = input.length - 1; i >= 0; i--) {
+			console.log(input[0]);
+		}
+	}
+	else {console.log("No devices found")}
+});
--- devices/usr-serial/discoverusr.js
+++ devices/usr-serial/discoverusr.js
@@ -3,38 +3,41 @@ const dgram = require('dgram');
 
 const PORT = 48899;
 const HOST = '255.255.255.255';
-const TIMEOUT = 3000;
+//const TIMEOUT = 3000;
 
 const message = new Buffer('www.usr.cn');
 const upd_client = dgram.createSocket('udp4');
 
-const timeoutObj = setTimeout(close_client, TIMEOUT);
+//var deviceinfo={IP:0,MAC:0}
+var answers = [];
 
 
-module.exports.discoverDevices = function() {
-
+module.exports.discoverDevices = function(timeout,callback) {
+	const timeoutObj = setTimeout(process_message, timeout);
 	upd_client.bind(()=>{upd_client.setBroadcast(true);});
 
 	upd_client.on('message',(msg,info)=>{
-		clearTimeout(timeoutObj);
-		close_client(msg,info);
+		//clearTimeout(timeoutObj);
+		answers.push(msg.toString().slice(0, -1).split(","));
 	});
 
 	upd_client.send(message, 0, message.length, PORT, HOST, (err, bytes) => {
 		if (err) throw err;
 		//console.log('UDP message sent to ' + HOST +':'+ PORT);
-	});
-}
 
-function close_client(msg,info){
-	//console.log('Received %d bytes from %s:%d Message: "%s"\n',msg.length, info.address, info.port,msg.toString());
-	if(msg!=undefined && info!=undefined){
-		console.log(info.address);
+	});
+
+	function process_message(){
+		//console.log('Received %d bytes from %s:%d Message: "%s"\n',msg.length, info.address, info.port,msg.toString());
+		/*if(msg!=undefined && info!=undefined){
+			console.log(info.address);
+		}
+		else{
+			console.log('Serial Converter unreachable')
+		}*/
+		upd_client.close();
+		callback(answers);
 	}
-	else{
-		console.log('Serial Converter unreachable')
-	}
-	upd_client.close();
 }
 
 
